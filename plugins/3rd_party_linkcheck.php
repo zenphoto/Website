66c3720369ef56794f2a3371c12005f76cdbf905
Stephen Billard <stephen@sbillard.org>
2013-02-16 14:23:42 -0800

    fix plugin so it does not throw errors

--- plugins/3rd_party_linkcheck.php
+++ plugins/3rd_party_linkcheck.php
@@ -2,29 +2,37 @@
 /**
  * 3rd party link checker
  *
- * Checks links of 3rd party plugins and themes regarding accessibility. 
- * If they are not the theme or plugin is considereed to be abandoned. 
- * The entries then are marked unpublished and marked with the tag 
+ * Checks links of 3rd party plugins and themes regarding accessibility.
+ * If they are not the theme or plugin is considereed to be abandoned.
+ * The entries then are marked unpublished and marked with the tag
  * <em>theme-abandoned</em> respectively <em>extension-abandoned</em>
  *
  * @package admin
  * @subpackage Zenphoto
  */
-$plugin_is_filter = 20|ADMIN_PLUGIN; 
+$plugin_is_filter = 20|ADMIN_PLUGIN;
 $plugin_description = gettext('Checks info/download links of 3rd party theme and extensions entries. For Zenphoto.org only.');
 $plugin_author = "Malte Müller (acrylian)";
 $plugin_version = '1.4.4';
-$option_interface = 'linkchecker';
 
 zp_register_filter('admin_utilities_buttons', 'linkcheckButton');
-/* 
+
+/*
 $linkcheckinterval = getOption('zenphoto_linkcheck_interval');
 $lastlinkcheck = getOption('zenphoto_linkcheck_last');
 if(time()-$lastlinkcheck < $linkcheckinterval) {
 	setOption('zenphoto_linkcheck_last',time());
 	zp_register_filter('theme_head','checkThemesAndExtensionURLs');
-} */
-/* 
+}
+*********************************************************
+Note: the asterix on option_interface below is to keep the admin pages from thinking there is an option handler for this plugin.
+Perhaps a better thing would to be either:
+a. remove all this code if it is not to be used
+or
+b. allow a real options handler but disable all the options.
+*********************************************************
+*option_interface = 'linkchecker';
+
 class linkchecker {
 	function linkchecker() {
 		setOptionDefault('zenphoto_linkcheck_interval', 604800);
