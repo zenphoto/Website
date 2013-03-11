899d823f979c1b94e3a4fde8bfae8e04680d2275
Stephen Billard <stephen@sbillard.org>
2013-03-11 15:39:16 -0700

    Plugin to set the demo site title to include the zenphoto version

---
+++ plugins/demoTitle.php
@@ -0,0 +1,12 @@
+<?php
+/** sets the titl of the gallery to include the version
+ *
+ * @package plugins
+ * @subpackage Zenphoto
+ */
+$plugin_is_filter = 5|THEME_PLUGIN;
+$plugin_description = gettext('Provides a dynamic gallery title.');
+$plugin_author = "Stephen Billard (sbillard)";
+
+$_zp_gallery->setTitle('Zenphoto v'.ZENPHOTO_VERSION.' demo');
+?>
