3693a7e331d072b9c816d76e6580b5cc5c3e0d59
acrylian <wartgart@gmx.net>
2014-03-30 10:34:34 +0200

    html5 charset meta tag

--- zenphoto-theme/header.php
+++ zenphoto-theme/header.php
@@ -9,6 +9,7 @@ header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 	<head>
 		<?php zp_apply_filter('theme_head'); ?>
 		<title><?php echo zp_printPageHeaderTitle(); ?></title>
+		<meta charset="<?php echo LOCAL_CHARSET; ?>">
 		<meta http-equiv="content-type" content="text/html; charset=<?php echo getOption('charset'); ?>" />
 		<!-- touch icons for tablet and smartphone bookmarks -->
 		<link rel="apple-touch-icon" href="<?php echo $_zp_themeroot; ?>/images/apple-touch-icon.png" />
