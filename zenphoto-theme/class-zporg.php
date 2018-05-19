39cb72bf438c069b7a48b5f59980280f303b0d21
acrylian <wartgart@gmx.net>
2018-05-19 15:26:06 +0200

    Fix cc logo

--- zenphoto-theme/class-zporg.php
+++ zenphoto-theme/class-zporg.php
@@ -1720,9 +1720,10 @@ class zporg {
 			}
 
 			static function printLicenseNote() {
+				global $_zp_themeroot;
 				?>
 				<div class="articlebox license">
-					<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a>This text by <a xmlns:cc="http://creativecommons.org/ns#" href="www.zenphoto.org" property="cc:attributionName" rel="cc:attributionURL">www.zenphoto.org</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</p>
+					<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="<?php echo $_zp_themeroot; ?>/images/cc-by-sa-88x31.png" /></a>This text by <a xmlns:cc="http://creativecommons.org/ns#" href="www.zenphoto.org" property="cc:attributionName" rel="cc:attributionURL">www.zenphoto.org</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</p>
 					<p>Code examples are released under the <a href="http://www.gnu.org/licenses/gpl-2.0.html">GPL v2 or later license</a></p>
 				</div>
 				<?php
